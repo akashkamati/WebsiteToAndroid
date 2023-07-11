92bb94454afb79702509b125f35cd2c43ad0d87f
akash <akashkamati246@gmail.com>
2023-07-11 16:19:52 +0530

    added splash screen support

--- app/src/main/java/com/websitetoandroid/MainActivity.kt
+++ app/src/main/java/com/websitetoandroid/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.websitetoandroid.ui.theme.WebsiteToAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             WebsiteToAndroidTheme {
                 Surface(
